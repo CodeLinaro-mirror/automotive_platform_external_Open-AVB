faa4fd19017876753f8e19d785e8765b0c360ea1
Zou Shunxiang <shunxian@codeaurora.org>
2020-07-16 10:46:20 +0800

    Open-avb: update common_port for enhancement
    use unlock APIs to avoid underline dead lock issue.
    
    Change-Id: Ica28e2702012d06c490739ad1e577dd51fb2f463
    Signed-off-by: Zou Shunxiang <shunxian@codeaurora.org>

--- daemons/gptp/common/avbts_clock.hpp
+++ daemons/gptp/common/avbts_clock.hpp
@@ -162,6 +162,7 @@ private:
 
     OSLock *timerq_lock;
 
+public:
 	/**
 	 * @brief  Add a new event to the timer queue
 	 * @param  target EtherPort target
@@ -179,7 +180,7 @@ private:
 	 * @return void
 	 */
 	void deleteEventTimer( CommonPort *target, Event e );
-public:
+
   /**
    * @brief Instantiates a IEEE 1588 Clock
    * @param forceOrdinarySlave Forces it to be an ordinary slave
