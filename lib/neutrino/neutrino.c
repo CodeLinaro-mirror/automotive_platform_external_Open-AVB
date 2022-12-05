CodeLinaro-mirror/femto5g_platform_external_Open-AVB

open-avb: Fix kw open-avb issue
Change-Id: I5a37da096da6c85bb9ad42e8343bc3ec13c25eb4
Signed-off-by: Zou Shunxiang <shunxian@codeaurora.org>

lib/neutrino/neutrino.c
@@ -181,7 +181,7 @@ int ntn_set_class_bandwidth(int nClass, unsigned classBytesPerSec, char *ifname)
 
 	avb_struct.op_mode = QAVB;
 	data.ptr = &avb_struct;
-	strncpy(ifr.ifr_ifrn.ifrn_name, ifname, IFNAMSIZ - 1);
+	snprintf(ifr.ifr_ifrn.ifrn_name, sizeof(ifr.ifr_ifrn.ifrn_name), "%s", ifname);
 	ifr.ifr_ifru.ifru_data = (void *)&data;
 
 	errno = 0;

CodeLinaro-mirror/automotive_platform_external_Open-AVB

lib/neutrino/neutrino.c
@@ -190,7 +190,7 @@ int ntn_set_class_bandwidth(int nClass, unsigned classBytesPerSec, char *ifname)
 
 	avb_struct.op_mode = QAVB;
 	data.ptr = &avb_struct;
-	strncpy(ifr.ifr_ifrn.ifrn_name, ifname_interface, IFNAMSIZ - 1);
+	snprintf(ifr.ifr_ifrn.ifrn_name, sizeof(ifr.ifr_ifrn.ifrn_name), "%s", ifname_interface);
 	ifr.ifr_ifru.ifru_data = (void *)&data;
 
 	errno = 0;